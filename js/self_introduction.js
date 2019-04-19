4d33739742471602054a0dc5c8f6f6d65de6833d
genio_karthik <karthik@genio.co.jp>
2019-04-19 15:06:58 +0900

    [fix]：日本語メニューの対応

--- js/self_introduction.js
+++ js/self_introduction.js
@@ -1,15 +1,18 @@
 $(function(){
 
+	// タブメニューを押下時
 	$('#tabMenu li a').on('click', function() {
 		$(this).closest('ul').find('li').removeClass('current');
 		$(this).parent().addClass('current');
 
+		// toggleで移動
 		var str_href = $(this).attr("href");
 		if (str_href != '' && str_href != undefined) {
 			$("#tabBoxes div.content").hide();
 			$($(this).attr("href")).fadeToggle();
 		} else if (str_href == '') {
 			$("#tabBoxes div.content").show();
+			$("#jaHome").hide();
 		}
 
 		return false;
